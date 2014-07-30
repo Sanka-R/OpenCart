cd84bc54a0a13d6bba642294331c2278e79ece74
SankaGIT <nuwan.ras@gmail.com>
2014-07-30 14:21:17 +0530

    Initial Commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/upload/admin/');
+define('DIR_SYSTEM', '/var/www/opencart/upload/system/');
+define('DIR_DATABASE', '/var/www/opencart/upload/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencart/upload/admin/language/');
+define('DIR_TEMPLATE', '/var/www/opencart/upload/admin/view/template/');
+define('DIR_CONFIG', '/var/www/opencart/upload/system/config/');
+define('DIR_IMAGE', '/var/www/opencart/upload/image/');
+define('DIR_CACHE', '/var/www/opencart/upload/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart/upload/download/');
+define('DIR_LOGS', '/var/www/opencart/upload/system/logs/');
+define('DIR_CATALOG', '/var/www/opencart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '1234');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/opencart/upload/system/');
+define('DIR_DATABASE', '/var/www/opencart/upload/system/database/');
+define('DIR_LANGUAGE', '/var/www/opencart/upload/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/opencart/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/opencart/upload/system/config/');
+define('DIR_IMAGE', '/var/www/opencart/upload/image/');
+define('DIR_CACHE', '/var/www/opencart/upload/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/opencart/upload/download/');
+define('DIR_LOGS', '/var/www/opencart/upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '1234');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
